60fe623b33b8c5204b263b3aa63f74678125ea50
danielcavalcante99 <daniel16henrrique@gmail.com>
2023-11-14 01:20:57 -0300

    Criando CommentRequestDTO

---
+++ src/main/java/br/com/projectblog/dtos/requests/CommentRequestDTO.java
@@ -0,0 +1,32 @@
+package br.com.projectblog.dtos.requests;
+
+import java.io.Serializable;
+
+import br.com.projectblog.validations.groups.OnCreate;
+import br.com.projectblog.validations.groups.OnUpdate;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class CommentRequestDTO implements Serializable{
+
+	private static final long serialVersionUID = 1346226928441358809L;
+	
+	@Null(groups = OnCreate.class, message = "O campo 'commentId' não deve ser informado na inclusão.")
+	@NotNull(groups = OnUpdate.class, message = "O campo 'commentId' é obrigatório ser informado na atualização.")
+	private String commentId;
+
+	@NotNull(groups = {OnCreate.class, OnUpdate.class}, message = "O campo 'postId' é obrigatório.")
+	private String postId;
+	
+	@NotNull(groups = {OnCreate.class, OnUpdate.class}, message = "O campo 'userId' é obrigatório.")
+	private String userId;
+	
+	private String observation;
+
+}
